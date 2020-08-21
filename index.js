1b42b4cd8ea1cc016186527f8d4c0969747992c6
taiwocoker <taiwofcoker@gmail.com>
2020-08-21 11:08:51 +0100

    add the render function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let myLibrary = [];
+let mybooks = document.getElementsByClassName('book-card');
+
+
 
 function Book(title, author, pages, read){ 
     this.title = title; 
@@ -18,6 +21,24 @@ function addBookToLibrary(title, author, pages, read) {
 
 function render() {
     for(let i=0; i<myLibrary.length; i++){
-        
+        mybooks.innerHTML = `<div class="col-4 p-0 card text-center light-background">
+        <div class="card-header">
+          ${myLibrary[i].author}
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${myLibrary[i].title}</h5>
+          <p class="card-text">${myLibrary[i].pages} pages</p>
+          <a href="#" class="btn btn-primary">Remove this book</a>
+        </div>
+        <div class="card-footer text-muted">
+        ${myLibrary[i].read}
+        </div>
+      </div>`
     }
 }
+
+addBookToLibrary('Math', 'Taiwo', 200, 'Read');
+addBookToLibrary('JavaScript', 'Patricia', 300, 'Read');
+addBookToLibrary('Romeo and Juliet', 'Patricia', 500, 'Not Read yet');
+render(); 
+ 
