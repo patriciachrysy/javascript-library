b68336ca0718078cf87c88ad1186ee292bc645b7
Manezeu Patricia <pmanezeu@gmail.com>
2020-08-21 18:25:37 +0100

    fixed the linters errors and the toggleRead error

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let myLibrary = localStorage.getItem('library') ? JSON.parse(localStorage.getItem('library')) : [];
-let mybooks = document.querySelector('.book-card');
-let formSpace = document.querySelector('.form-space');
-let bookForm = document.getElementById('display-form');
- bookForm.addEventListener('click', function(){
-    formSpace.innerHTML = `
+/*
+  eslint-disable no-unused-vars, no-alert
+*/
+const myLibrary = localStorage.getItem('library') ? JSON.parse(localStorage.getItem('library')) : [];
+const mybooks = document.querySelector('.book-card');
+const formSpace = document.querySelector('.form-space');
+const bookForm = document.getElementById('display-form');
+bookForm.addEventListener('click', () => {
+  formSpace.innerHTML = `
     <div class="col-8 p-3 card light-background">
         <form id="bookForm">
             <div class="form-group">
@@ -28,40 +31,37 @@ let bookForm = document.getElementById('display-form');
             <button type="button" class="btn btn-primary" id="addBook" onclick="saveBook()">Save</button>
         </form>
     </div>
-`
+`;
 });
 
 
-function Book(title, author, pages, read){ 
-    this.title = title; 
-    this.author = author; 
-    this.pages = pages; 
-    this.read = read; 
-    this.info = function(){
-        return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read; 
-    } 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-Book.prototype.toggleRead = function(){
-    if (this.read == "Read"){
-        this.read = "Not read yet";
-    }else{
-        this.read = "Read";
-    }
-    render();
-}
+Book.prototype.toggleRead = function () {
+  if (this.read === 'Read') {
+    this.read = 'Not read yet';
+  } else {
+    this.read = 'Read';
+  }
+};
 
 
 function addBookToLibrary(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-    localStorage.setItem('library', JSON.stringify(myLibrary));
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
 function render() {
-    let allBooks = '';
-        for(let i=0; i<myLibrary.length; i++){
-            allBooks += `<div class="col-3 p-0 m-2 card text-center light-background">
+  let allBooks = '';
+  let i = 0;
+  while (i < myLibrary.length) {
+    allBooks += `<div class="col-3 p-0 m-2 card text-center light-background">
             <div class="card-header">
             ${myLibrary[i].author}
             </div>
@@ -74,37 +74,43 @@ function render() {
                 <a href="#" class="btn btn-info" onclick="changeRead(${i})">${myLibrary[i].read}</a>
             </div>
         </div>`;
-        }
-    mybooks.innerHTML = allBooks;
+    i += 1;
+  }
+
+  mybooks.innerHTML = allBooks;
 }
 
-function changeRead(index){
-    let mybook = myLibrary[index];
-    mybook.toggleRead();
+function changeRead(index) {
+  const mybook = myLibrary[index];
+  if (mybook.read === 'Read') {
+    mybook.read = 'Not read yet';
+  } else {
+    mybook.read = 'Read';
+  }
+  localStorage.setItem('library', JSON.stringify(myLibrary));
+  render();
 }
 
-function removeBook(index){
-    let confirmation = confirm(`Are you sure you want to remove ${myLibrary[index].title}?`)
-        if(confirmation == true){
-            myLibrary.splice(index,1);
-            render();
-    }
+function removeBook(index) {
+  const confirmation = window.confirm(`Are you sure you want to remove ${myLibrary[index].title}?`);
+  if (confirmation === true) {
+    myLibrary.splice(index, 1);
+    render();
+  }
 
-    localStorage.setItem('library', JSON.stringify(myLibrary));
+  localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
-function saveBook(){
-    let myForm = document.getElementById('bookForm');
-    if (myForm.elements.namedItem('title').value && myForm.elements.namedItem('author').value && myForm.elements.namedItem('pages').value && myForm.elements.namedItem('read').value){
-
-        addBookToLibrary(myForm.elements.namedItem('title').value, myForm.elements.namedItem('author').value, myForm.elements.namedItem('pages').value, myForm.elements.namedItem('read').value);
-        render();
-        myForm.reset();
-        formSpace.innerHTML = '';
-    }else {
-        alert("Please fill all the fields correctly");
-    }
+function saveBook() {
+  const myForm = document.getElementById('bookForm');
+  if (myForm.elements.namedItem('title').value && myForm.elements.namedItem('author').value && myForm.elements.namedItem('pages').value && myForm.elements.namedItem('read').value) {
+    addBookToLibrary(myForm.elements.namedItem('title').value, myForm.elements.namedItem('author').value, myForm.elements.namedItem('pages').value, myForm.elements.namedItem('read').value);
+    render();
+    myForm.reset();
+    formSpace.innerHTML = '';
+  } else {
+    alert('Please fill all the fields correctly');
+  }
 }
 
-render(); 
-console.log(localStorage.getItem('library'));
+render();
