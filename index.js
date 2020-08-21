82a23efc552fe4ffce98b5c063ada571716a46be
Manezeu Patricia <pmanezeu@gmail.com>
2020-08-21 16:53:22 +0100

    Used localStorage to store the books locally

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let myLibrary = [];
+let myLibrary = localStorage.getItem('library') ? JSON.parse(localStorage.getItem('library')) : [];
 let mybooks = document.querySelector('.book-card');
 let formSpace = document.querySelector('.form-space');
 let bookForm = document.getElementById('display-form');
  bookForm.addEventListener('click', function(){
     formSpace.innerHTML = `
     <div class="col-8 p-3 card light-background">
-        <form id="bookForm" action="#">
+        <form id="bookForm">
             <div class="form-group">
                 <label for="title">The book title</label>
                 <input type="text" name="title" class="form-control" placeholder="Enter the book title" required>
@@ -25,7 +25,7 @@ let bookForm = document.getElementById('display-form');
                     <option>Not read yet</option>
                 </select>
             </div>
-            <button type="submit" class="btn btn-primary" id="addBook" onclick="saveBook()">Save</button>
+            <button type="button" class="btn btn-primary" id="addBook" onclick="saveBook()">Save</button>
         </form>
     </div>
 `
@@ -55,25 +55,26 @@ Book.prototype.toggleRead = function(){
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+    localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
 function render() {
     let allBooks = '';
-    for(let i=0; i<myLibrary.length; i++){
-        allBooks += `<div class="col-3 p-0 card text-center light-background">
-        <div class="card-header">
-          ${myLibrary[i].author}
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">${myLibrary[i].title}</h5>
-          <p class="card-text">${myLibrary[i].pages} pages</p>
-          <a href="#" class="btn btn-warning" onclick="removeBook(${i})">Remove this book</a>
-        </div>
-        <div class="card-footer text-muted">
-            <a href="#" class="btn btn-info" onclick="changeRead(${i})">${myLibrary[i].read}</a>
-        </div>
-      </div>`;
-    }
+        for(let i=0; i<myLibrary.length; i++){
+            allBooks += `<div class="col-3 p-0 m-2 card text-center light-background">
+            <div class="card-header">
+            ${myLibrary[i].author}
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">${myLibrary[i].title}</h5>
+            <p class="card-text">${myLibrary[i].pages} pages</p>
+            <a href="#" class="btn btn-warning" onclick="removeBook(${i})">Remove this book</a>
+            </div>
+            <div class="card-footer text-muted">
+                <a href="#" class="btn btn-info" onclick="changeRead(${i})">${myLibrary[i].read}</a>
+            </div>
+        </div>`;
+        }
     mybooks.innerHTML = allBooks;
 }
 
@@ -88,19 +89,22 @@ function removeBook(index){
             myLibrary.splice(index,1);
             render();
     }
+
+    localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
 function saveBook(){
     let myForm = document.getElementById('bookForm');
-    addBookToLibrary(myForm.elements.namedItem('title').value, myForm.elements.namedItem('author').value, myForm.elements.namedItem('pages').value, myForm.elements.namedItem('read').value);
-    render();
-    myForm.reset();
-    formSpace.setAttribute('style', 'display: none !important');
+    if (myForm.elements.namedItem('title').value && myForm.elements.namedItem('author').value && myForm.elements.namedItem('pages').value && myForm.elements.namedItem('read').value){
+
+        addBookToLibrary(myForm.elements.namedItem('title').value, myForm.elements.namedItem('author').value, myForm.elements.namedItem('pages').value, myForm.elements.namedItem('read').value);
+        render();
+        myForm.reset();
+        formSpace.innerHTML = '';
+    }else {
+        alert("Please fill all the fields correctly");
+    }
 }
 
-addBookToLibrary('Math', 'Taiwo', 200, 'Read');
-addBookToLibrary('JavaScript', 'Patricia', 300, 'Read');
-addBookToLibrary('Romeo and Juliet', 'Patricia', 500, 'Not Read yet');
 render(); 
-console.log(myLibrary);
-
+console.log(localStorage.getItem('library'));
